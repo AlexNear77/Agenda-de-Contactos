7c0d612ec5f0a914d55159a289989149543d9b22
Alex <alexgomeznear@gmail.com>
2020-09-01 01:49:03 -0500

    Creando el proyecto y colocando css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment} from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {/*----------------------------------------------------- */}
+      {/*                         TITULO                       */}
+      {/*======================================================*/}
+
+      <nav className="navbar navbar-expand-lg  bg-dark">
+        <div className="container justify-content-center">
+          <h3 className="text-white">Contactos</h3>
+        </div>
+      </nav>
+      
+    </Fragment>
   );
 }
 
