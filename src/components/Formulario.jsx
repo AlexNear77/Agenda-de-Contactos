0bb864cef504b3f4056588784abce502ef8ea0eb
Alex <alexgomeznear@gmail.com>
2020-09-05 01:41:41 -0500

    Creando campos del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 
 const Formulario = () => {
    return ( 
-      <h1>Desde Formulario</h1>
+      <Fragment>
+         {/*                     FORMULARIO                     */}
+         <div className="card ">
+            <h4 className="card-header">Agregar Tarea</h4>
+            <div className="card-body">
+            <form >
+               {/*                        Nombre                         */}
+               <div className="form-group">
+                  <label >Nombre:</label>
+                  <input type="text"
+                        name="contacto"
+                        className="form-control"
+                        placeholder="Nombre del contacto"
+                  />
+               </div>
+               {/*                        edad                    */}
+               <div className="form-group">
+                  <label >Numero:</label>
+                  <input type="text"
+                        name="numero"
+                        className="form-control"
+                        placeholder="Numero del contacto"
+                  />
+               </div>
+               
+               {/*                        Button                    */}
+               <button type="submit"
+                     className="btn btn-success btn-lg btn-block">
+                        Agregar Contacto
+               </button>
+            </form>
+            </div>
+         </div>
+      </Fragment>
     );
 }
  
